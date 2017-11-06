31f93867341a7b2d2215a6bb32a2e5cf15664111
Mickael Villette <mickael.villette@atos.net>
2017-11-06 14:39:30 +0100

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name a577435App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the a577435App
+ */
+angular.module('a577435App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
